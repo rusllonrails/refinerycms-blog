4df6d15f9cf10d83adca247effb5a1a5a6e825dc
Ruslan Khamidullin <rusllonrails@gmail.com>
2017-06-28 11:59:27 +0300

    [BLOG WYSYWYG EDITOR] fix

--- app/assets/javascripts/refinery/blog/backend.js
+++ app/assets/javascripts/refinery/blog/backend.js
@@ -56,12 +56,7 @@ $(document).ready(function(){
   $('#copy_body_link').click(function(event) {
     // Find the WYMEditor that maps to the custom_teaser field
     var teaserTextArea = $('#post_custom_teaser')[0];
-    var teaserEditor = null;
-    $.each(WYMeditor.INSTANCES, function(index, editor) {
-      if (editor._element[0] == teaserTextArea) {
-        teaserEditor = editor;
-      }
-    });
+    var teaserEditor = tinymce.get('post_custom_teaser');
 
     if (teaserEditor) {
       teaserEditor.html($('#post_body').val());
